80aec4ed3f3bc49985952b28aba3442d07a140a7
gabrgarc <gabrgarc@student.42sp.org.br>
2025-12-22 16:20:04 -0300

    feature: function to transform environment variables into a table

---
+++ env/env_to_table.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_to_table.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/21 12:30:00 by gabrgarc          #+#    #+#             */
+/*   Updated: 2025/12/22 14:44:24 by gabrgarc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "hashtable.h"
+
+static char	*extract_key(char *env_line);
+static char	*extract_value(char *env_line);
+
+t_hash_table	*env_to_table(char **env)
+{
+	t_hash_table	*table;
+	char			*key;
+	char			*value;
+	int				i;
+
+	if (!env)
+		return (NULL);
+	table = create_table(257);
+	if (!table)
+		return (NULL);
+	i = 0;
+	while (env[i])
+	{
+		key = extract_key(env[i]);
+		value = extract_value(env[i]);
+		if (key && value)
+			hash_upsert(table, key, value);
+		free(key);
+		free(value);
+		i++;
+	}
+	return (table);
+}
+
+static char	*extract_key(char *env_line)
+{
+	char	*equal_sign;
+	char	*key;
+	int		key_len;
+
+	if (!env_line)
+		return (NULL);
+	equal_sign = ft_strchr(env_line, '=');
+	if (!equal_sign)
+	{
+		key = ft_strdup(env_line);
+		return (key);
+	}
+	key_len = equal_sign - env_line;
+	key = ft_substr(env_line, 0, key_len);
+	return (key);
+}
+
+static char	*extract_value(char *env_line)
+{
+	char	*equal_sign;
+	char	*value;
+
+	if (!env_line)
+		return (NULL);
+	equal_sign = ft_strchr(env_line, '=');
+	if (!equal_sign)
+	{
+		value = ft_strdup("");
+		return (value);
+	}
+	value = ft_strdup(equal_sign + 1);
+	return (value);
+}
