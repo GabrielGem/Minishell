2f3616b4eca7cf67fe2ebf40819def803f63b9d0
gabrgarc <gabrgarc@student.42sp.org.br>
2026-01-04 21:17:27 -0300

    organized order of includes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 09:58:27 by mmaquine          #+#    #+#             */
-/*   Updated: 2025/12/29 13:51:27 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2025/12/29 17:16:54 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <dirent.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 
+# include "libft.h"
+# include "hashtable.h"
 # include "structs.h"
 # include "parser.h"
-# include "libft.h"
 # include "environment.h"
-# include "hashtable.h"
 
 #endif
