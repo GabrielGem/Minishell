2e45d0f8c16ad7653c51a5e969777f9e2b869a6a
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-09 21:18:16 -0300

    norminette

--- srcs/utils/concatenate.c
+++ srcs/utils/concatenate.c
@@ -6,13 +6,13 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 10:42:47 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/23 17:24:47 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/09 16:28:15 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char **concatenate(char **strs, char *join)
+char	**concatenate(char **strs, char *join)
 {
 	char	*temp;
 	int		i;
