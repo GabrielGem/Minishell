91dd8b140fd1d54fff561a46ba69bcaea4d72df4
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-09 21:11:57 -0300

    norminette + change return type

--- includes/executor.h
+++ includes/executor.h
@@ -6,15 +6,15 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 17:47:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/07 17:48:51 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/09 16:25:50 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXECUTOR_H
 # define EXECUTOR_H
 
-#include "libft.h"
+# include "libft.h"
 
-int	handle_redirs(t_list *lst);
+int	*handle_redirs(t_list *lst);
 
 #endif
