975a05f5eae7672a64b0daac9b74c76c739b7be4
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-09 21:10:29 -0300

    norminette

--- includes/environment.h
+++ includes/environment.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 14:26:12 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/24 14:46:11 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/09 16:25:26 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include "hashtable.h"
 
 char			*binary_search(char **path, char *cmd);
-char		 	**concatenate(char **strs, char *join);
+char			**concatenate(char **strs, char *join);
 t_hash_table	*env_to_table(char **env);
 int				is_valid_executable(char *path);
 char			**table_to_env(t_hash_table *table);
