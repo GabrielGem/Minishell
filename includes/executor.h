5bd9777599cf33e7c22a7ca7f19d1417b6628997
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-16 14:46:36 -0300

    change prototypes

--- includes/executor.h
+++ includes/executor.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 17:47:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/15 17:29:05 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/16 11:23:25 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 
 # include "libft.h"
 
-typedef int	(*t_handle)(t_node *leaf, t_data *context);
+typedef int	(*t_handle)(t_ast_node *leaf, t_data *context);
 
 int	*handle_redirs(t_list *lst);
-int	executor(t_node *leaf, t_data *context);
-int	handle_command(t_node *leaf, t_data *context);
+int	executor(t_data *context);
+int	handle_command(t_ast_node *leaf, t_data *context);
 
 #endif
