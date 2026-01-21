34ece0adfe92e4a5b2b9e2d7f91a3bc503a80965
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-21 17:03:12 -0300

    add: function pipe prototype

--- includes/executor.h
+++ includes/executor.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 17:47:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/16 11:23:25 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/21 16:57:15 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ typedef int	(*t_handle)(t_ast_node *leaf, t_data *context);
 int	*handle_redirs(t_list *lst);
 int	executor(t_data *context);
 int	handle_command(t_ast_node *leaf, t_data *context);
+int	handle_pipe(t_ast_node *leaf, t_data *context);
 
 #endif
