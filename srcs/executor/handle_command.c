2bf3759e32088516ab2ab4c2ecdfb26d01750c64
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-21 22:58:55 -0300

    fix: redirs invalid read and return, add: perror & exit 127

--- srcs/executor/handle_command.c
+++ srcs/executor/handle_command.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 21:22:38 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/16 21:53:49 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/21 22:52:12 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,33 @@ int	handle_command(t_ast_node *leaf, t_data *context)
 	int			*redirs;
 
 	cmd = (t_command *)leaf;
-	status = 0;
 	id = fork();
 	if (id == -1)
 		return (-1);
 	if (id == 0)
 	{
-		redirs = NULL;
 		if (cmd->redirects != NULL)
 		{
 			redirs = handle_redirs(cmd->redirects);
 			if (redirs[REDIN] == 1 || redirs[REDOUT] == 1)
-				return (1);
+				exit(1);
 			if (redirs[REDIN] != -1)
 				dup2(redirs[REDIN], STDIN_FILENO);
 			if (redirs[REDOUT] != -1)
 				dup2(redirs[REDOUT], STDOUT_FILENO);
+			close(redirs[REDIN]);
+			close(redirs[REDOUT]);
 		}
-		close(redirs[REDIN]);
-		close(redirs[REDOUT]);
 		child_process(cmd->args, context->envp);
 	}
+	status = 0;
 	waitpid(id, &status, 0);
-	return (status);
+	return (WEXITSTATUS(status));
 }
 
 static void	child_process(char **args, char **envp)
 {
 	execve(args[0], &args[1], envp);
+	perror("execve");
+	exit(127);
 }
