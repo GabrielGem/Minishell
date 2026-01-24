636a86f0f8c6759600e0e690a770aaf2735c5925
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-24 18:07:32 -0300

    The function no longer swaps the entire program's data sources and stores them in a list.

--- srcs/executor/handle_pipe.c
+++ srcs/executor/handle_pipe.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 10:34:18 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/22 19:28:42 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/23 19:22:11 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,26 @@ static int	pipe_recursive(t_ast_node *leaf, int input_fd, int output_fd, \
 
 int	handle_pipe(t_ast_node *leaf, t_data *context)
 {
-	pipe_recursive(leaf, STDIN_FILENO, STDOUT_FILENO, context);
-	return (0);
+	context->fds = ft_lstnew((void *)(long)123456789);
+	return (pipe_recursive(leaf, STDIN_FILENO, STDOUT_FILENO, context));
 }
 
 static int	pipe_recursive(t_ast_node *leaf, int input_fd, int output_fd, \
 	t_data *context)
 {
 	int		pipefd[2];
+	int		status;
 
+	if (leaf->type.base == NODE_COMMAND)
+		return (handle_command_fd(leaf, input_fd, output_fd, context));
 	if (leaf->type.base == NODE_PIPE)
 	{
-		int	stdout_backup = dup(output_fd);
 		pipe(pipefd);
+		ft_lstadd_back(&context->fds, ft_lstnew((void *)(long)pipefd[0]));
+		ft_lstadd_back(&context->fds, ft_lstnew((void *)(long)pipefd[1]));
 		pipe_recursive((t_ast_node *)leaf->left, input_fd, pipefd[1], context);
-		close(pipefd[1]);
-		dup2(stdout_backup, STDOUT_FILENO);
-		close(stdout_backup);
-		pipe_recursive((t_ast_node *)leaf->right, pipefd[0], output_fd, context);
-		close(pipefd[0]);
-		if (input_fd != STDIN_FILENO)
-			close(input_fd);
-		if (output_fd != STDOUT_FILENO)
-			close(output_fd);
+		status = pipe_recursive((t_ast_node *)leaf->right, pipefd[0], \
+			output_fd, context);
 	}
-	else if (leaf->type.base == NODE_COMMAND)
-	{
-		if (input_fd != STDIN_FILENO)
-		{
-			dup2(input_fd, STDIN_FILENO);
-			close(input_fd);
-		}
-		if (output_fd != STDOUT_FILENO)
-		{
-			dup2(output_fd, STDOUT_FILENO);
-			close(output_fd);
-		}
-		executor(leaf, context);
-	}
-	return (0);
+	return (status);
 }
