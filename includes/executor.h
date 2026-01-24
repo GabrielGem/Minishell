20730a1bde378ad833b7d83b4a8edde9b6137404
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-24 18:22:40 -0300

    add: prototypes command fd and free context

--- includes/executor.h
+++ includes/executor.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 17:47:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/21 22:43:10 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/24 18:20:59 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@ typedef int	(*t_handle)(t_ast_node *leaf, t_data *context);
 int	*handle_redirs(t_list *lst);
 int	executor(t_ast_node *root, t_data *context);
 int	handle_command(t_ast_node *leaf, t_data *context);
+int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, t_data *context);
 int	handle_pipe(t_ast_node *leaf, t_data *context);
 
+void	free_context(t_data *context);
 void	free_tree(t_ast_node *tree);
 
 #endif
