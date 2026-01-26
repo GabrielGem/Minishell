224c45e469e5cce9584b96e92a59fb551c6909bb
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-26 16:01:47 -0300

    separate functions

--- srcs/executor/handle_command.c
+++ srcs/executor/handle_command.c
@@ -6,26 +6,28 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 21:22:38 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/23 20:40:43 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/26 15:56:17 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
+int			handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
 	t_data *context);
+static void	duplicate_fd(int new_fd, int old_fd);
+static void	redirects(t_list *redirs);
 
 int	handle_command(t_ast_node *leaf, t_data *context)
 {
 	return (handle_command_fd(leaf, STDIN_FILENO, STDOUT_FILENO, context));
 }
 
-int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, t_data *context)
+int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
+	t_data *context)
 {
 	t_command	*cmd;
 	pid_t		id;
 	int			status;
-	int			*redirs;
 
 	cmd = (t_command *)leaf;
 	id = fork();
@@ -34,30 +36,11 @@ int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, t_data *con
 	if (id == 0)
 	{
 		if (input_fd != STDIN_FILENO)
-		{
-			dup2(input_fd, STDIN_FILENO);
-			close(input_fd);
-		}
+			duplicate_fd(input_fd, STDIN_FILENO);
 		if (output_fd != STDOUT_FILENO)
-		{
-			dup2(output_fd, STDOUT_FILENO);
-			close(output_fd);
-		}
+			duplicate_fd(output_fd, STDOUT_FILENO);
 		if (cmd->redirects != NULL)
-		{
-			redirs = handle_redirs(cmd->redirects);
-			if (redirs[REDIN] == 1 || redirs[REDOUT] == 1)
-			{
-				free_context(context);
-				exit(1);
-			}
-			if (redirs[REDIN] != -1)
-				dup2(redirs[REDIN], STDIN_FILENO);
-			if (redirs[REDOUT] != -1)
-				dup2(redirs[REDOUT], STDOUT_FILENO);
-			close(redirs[REDIN]);
-			close(redirs[REDOUT]);
-		}
+			redirects(cmd->redirs);
 		execve(cmd->args[0], &cmd->args[1], context->envp);
 		ft_putstr_fd("minishell: ", 2);
 		perror(cmd->args[0]);
@@ -69,6 +52,28 @@ int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, t_data *con
 	if (output_fd != STDOUT_FILENO)
 		close(output_fd);
 	status = 0;
-	waitpid(id, &status, 0);
+	waitpid(id, &status, WNOHANG);
 	return (WEXITSTATUS(status));
+}
+
+static void	duplicate_fd(int new_fd, int old_fd)
+{
+	dup2(new_fd, old_fd);
+	close(new_fd);
+}
+
+static void	redirects(t_list *redirs)
+{
+	int	*fds;
+
+	fds = handle_redirs(redirs);
+	if (fds[REDIN] == 1 || fds[REDOUT] == 1)
+	{
+		free_context(context);
+		exit(1);
+	}
+	if (fds[REDIN] != -1)
+		duplicate_fd(fds[REDIN], STDIN_FILENO);
+	if (fds[REDOUT] != -1)
+		duplicate_fd(fds[REDOUT], STDOUT_FILENO);
 }
