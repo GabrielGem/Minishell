5d2d683d4b4f5c9760cf703b8fe881bf66166ddc
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-27 16:13:05 -0300

    add: cleaning list of pids

--- srcs/utils/free/free_context.c
+++ srcs/utils/free/free_context.c
@@ -6,24 +6,31 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 20:14:17 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/24 12:04:40 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/27 15:19:00 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 void	close_fd(void *fd);
+void	close_pid(void *pid);
 
 void	free_context(t_data *context)
 {
 	destroy_table(context->env);
 	//ft_free_split(context->envp);
-	//context->envp = NULL;
 	free_tree(context->root);
 	ft_lstclear(&context->fds, close_fd);
+	ft_lstclear(&context->pids, close_pid);
 }
 
 void	close_fd(void *fd)
 {
 	close((int)(long)fd);
+}
+
+void	close_pid(void *pid)
+{
+	(void)pid;
+	return ;
 }
