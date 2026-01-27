c0472e37a43b80eda4609dc585c431f8880c07e5
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-27 16:09:30 -0300

    add: prototype function wait processes

--- includes/executor.h
+++ includes/executor.h
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 17:47:46 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/24 18:20:59 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/27 14:11:16 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@
 
 typedef int	(*t_handle)(t_ast_node *leaf, t_data *context);
 
-int	*handle_redirs(t_list *lst);
 int	executor(t_ast_node *root, t_data *context);
 int	handle_command(t_ast_node *leaf, t_data *context);
 int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, t_data *context);
+int	*handle_redirs(t_list *lst);
 int	handle_pipe(t_ast_node *leaf, t_data *context);
+
+int	wait_processes(t_list *pids);
 
 void	free_context(t_data *context);
 void	free_tree(t_ast_node *tree);
