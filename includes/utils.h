e1f1ed32d411f3cd2ae50b5240580d0dcccddac9
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-01-31 17:45:01 -0300

    update: prototype new utils functions and export utils

--- includes/utils.h
+++ includes/utils.h
@@ -6,23 +6,36 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:18:09 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/29 15:24:51 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/01/31 17:11:45 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-#include "hashtable.h"
-#include "structs.h"
+# include "hashtable.h"
+# include "structs.h"
 
 void			free_context(t_data *context);
 void			free_tree(t_ast_node *tree);
 
 char			*binary_search(char **path, char *cmd);
-char			**concatenate(char **strs, char *join);
-t_hash_table	*env_to_table(char **env);
 int				is_valid_executable(char *path);
+
+char			**concatenate(char **strs, char *join);
+
+t_hash_table	*env_to_table(char **env);
 char			**table_to_env(t_hash_table *table);
+t_hash_item		**table_for_array_of_items(t_hash_table *table, int count,\
+				t_hash_type tag);
+int				count_items_by_tag(t_hash_table *table, t_hash_type tag);
+void			sort_array_of_items(t_hash_item **array, int count);
+t_hash_item		**get_sorted_export_items(t_hash_table *table);
+
+char			*extract_value(char *line);
+char			*extract_key(char *line);
+
+int				is_valid_identifier(char *str);
+void			print_invalid_identifier(char *identifier);
 
 #endif
