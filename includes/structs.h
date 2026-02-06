8445760237a324a0f8eddc689a7862337c8e6926
mcmaquine <mcmaquine@gmail.com>
2026-02-06 19:13:55 -0300

    updated structs.h on this branch

--- includes/structs.h
+++ includes/structs.h
@@ -6,18 +6,12 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 10:11:10 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/20 15:13:38 by mmaquine         ###   ########.fr       */
+/*   Updated: 2026/02/06 18:38:00 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
-
-typedef enum e_node_type
-{
-	NODE_COMMAND,
-	NODE_PIPE,
-}	t_node_type;
 
 typedef enum e_type_redir
 {
@@ -27,42 +21,57 @@ typedef enum e_type_redir
 	HEREDOC
 }	t_type_redir;
 
-typedef struct s_ast_node
+enum e_builtin
 {
-	t_node_type			type;
-	struct s_ast_node	*left;
-	struct s_ast_node	*right;
-	char				**param;
-}	t_ast_node;
+	STANDART,
+	ECHO,
+	CD,
+	PWD,
+	B_EXPORT,
+	UNSET,
+	B_ENV,
+	EXIT
+};
+
+typedef enum e_node_type
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+}	t_node_type;
 
 typedef struct s_redir
 {
 	t_type_redir	type;
 	char			*filename;
-	struct s_redir	*next;
 }	t_redir;
+
+typedef struct s_node
+{
+	t_node_type base;
+}	t_node;
+
+typedef struct s_ast_node
+{
+	t_node	type;
+	t_node	*left;
+	t_node	*right;
+}	t_ast_node;
 
 typedef struct s_command
 {
-	int		is_builtin;
+	t_node	type;
+	char	is_builtin;
 	char	**args;
-	t_redir	*redirects;
+	t_list	*redirects;
 }	t_command;
 
 typedef struct s_data
 {
 	t_hash_table	*env;
 	char			**envp;
+	t_ast_node		*root;
+	t_list			*fds;
+	t_list			*pids;
 	int				exit_status;
 }	t_data;
-
-typedef struct e_exec
-{
-	int	(*pipes)[2];
-	int	*pids;
-	int	cmd_count;
-	int	stdin_backup;
-	int	stdout_backup;
-}	t_exec;
-
 #endif
