630503f563f65a27313af799657879cce588b495
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-09 10:24:42 -0300

    fix: Paths were tested without a slash between PATH and command.

--- srcs/utils/binary_search.c
+++ srcs/utils/binary_search.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 10:09:42 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/23 17:24:39 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/07 15:48:27 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 char	*binary_search(char **path, char *cmd)
 {
 	char	*path_binary;
+	char	*temp;
 
 	while (*path)
 	{
-		path_binary = ft_strjoin(*path, cmd);
+		temp = ft_strjoin(*path, "/");
+		path_binary = ft_strjoin(temp, cmd);
+		free(temp);
 		if (is_valid_executable(path_binary))
 			return (path_binary);
 		free(path_binary);
