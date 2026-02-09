3b0f878bfef08e21e59fb56906d1da6d783ecb10
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-09 10:27:09 -0300

    rename: more explicit function that cleans everything

--- srcs/utils/free/free_shell.c
+++ srcs/utils/free/free_shell.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   free_context.c                                     :+:      :+:    :+:   */
+/*   free_shell.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 20:14:17 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/04 13:59:22 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/07 18:07:17 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <minishell.h>
+#include "minishell.h"
+#include "tests.h"
 
-void	close_fd(void *fd);
-void	close_pid(void *pid);
-
-void	free_context(t_data *context)
+void	free_shell(t_data *context)
 {
 	destroy_table(context->env);
-	//ft_free_split(context->envp);
+	context->env = NULL;
 	free_tree(context->root);
+	context->root = NULL;
 	ft_lstclear(&context->fds, close_fd);
 	ft_lstclear(&context->pids, close_pid);
 	close(context->stdin_backup);
 	close(context->stdout_backup);
+	free(context);
 }
 
 void	close_fd(void *fd)
