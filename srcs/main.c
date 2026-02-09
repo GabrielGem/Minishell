1a83a8b77a9466fac899665282de1d5db1308191
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-09 10:23:28 -0300

    separate functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,163 +6,51 @@
 /*   By: mmaquine <mmaquine@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 09:58:31 by mmaquine          #+#    #+#             */
-/*   Updated: 2026/02/04 14:16:19 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/07 17:22:38 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static t_ast_node	*build_tree_polimorphic(void);
-static void			print_tree(t_ast_node *tree);
-void				print_hash_table(t_hash_table *table);
-void				print_array_items(t_hash_item **array, int count);
+#include "tests.h"
 
 int	main(int argc __attribute__((unused)), char **argv __attribute__((unused)),\
 	char **envp)
 {
 	char	*line;
-	t_data	context;
+	t_data	*context;
 
-	context = (t_data){0};
-	context.env = env_to_table(envp);
-	context.stdin_backup = dup(STDIN_FILENO);
-	context.stdout_backup = dup(STDOUT_FILENO);
+	context = init_shell(envp);
 	while (1)
 	{
 		line = readline("$> ");
 		if (line == NULL)
 			break ;
-		context.root = build_tree_polimorphic();
-		print_tree(context.root);
-		executor(context.root, &context);
-		//print_hash_table(context.env);
-		free_context(&context);
+		context->root = build_tree_polimorphic();
+		print_tree(context->root);
+		executor(context->root, context);
+		free_context(context);
 		free(line);
 	}
+	free_shell(context);
 	return (0);
 }
 
-static t_ast_node	*build_tree_polimorphic(void)
+t_data	*init_shell(char **envp)
 {
-	t_command	*cmd;
-	cmd = ft_calloc(1, sizeof(t_command));
-	cmd->type.base = NODE_COMMAND;
-	cmd->is_builtin = B_EXPORT;
-	cmd->args = ft_calloc(3, sizeof(char *));
-	cmd->args[0] = ft_strdup("export");
-	cmd->args[1] = ft_strdup("b");
-	//cmd->args[2] = ft_strdup("teste.txt");
-	cmd->args[2] = NULL;
+	t_data	*context;
 
-	t_command	*cmd2;
-	cmd2 = ft_calloc(1, sizeof(t_command));
-	cmd2->type.base = NODE_COMMAND;
-	cmd2->args = ft_calloc(4, sizeof(char *));
-	cmd2->args[0] = ft_strdup("/usr/bin/wc");
-	cmd2->args[1] = ft_strdup("wc");
-	cmd2->args[2] = ft_strdup("-l");
-	cmd2->args[3] = NULL;
-
-	//t_command	*cmd3;
-	//cmd3 = ft_calloc(1, sizeof(t_command));
-	//cmd3->type.base = NODE_COMMAND;
-	//cmd3->args = ft_calloc(4, sizeof(char *));
-	//cmd3->args[0] = ft_strdup("/usr/bin/wc");
-	//cmd3->args[1] = ft_strdup("wc");
-	//cmd3->args[2] = ft_strdup("-l");
-	//cmd3->args[3] = NULL;
-
-	//t_redir	*redir;
-	//redir = ft_calloc(1, sizeof(t_redir));
-	//redir->type = REDOUT;
-	//redir->filename = ft_strdup("file1");
-	//ft_lstadd_back(&cmd->redirects, ft_lstnew(redir));
-
-	//t_redir	*redir2;
-	//redir2 = ft_calloc(1, sizeof(t_redir));
-	//redir2->type = REDIN;
-	//redir2->filename = ft_strdup("infile");
-	//ft_lstadd_back(&cmd->redirects, ft_lstnew(redir2));
-
-	t_ast_node	*pipe1;
-	//t_ast_node	*pipe2;
-
-	pipe1 = ft_calloc(1, sizeof(t_ast_node));
-	pipe1->type.base = NODE_PIPE;
-	pipe1->left = (t_node *)cmd;
-	pipe1->right = (t_node *)cmd2;
-	//pipe2 = ft_calloc(1, sizeof(t_ast_node));
-	//pipe2->type.base = NODE_PIPE;
-	//pipe2->left = (t_node *)pipe1;
-	//pipe2->right = (t_node *)cmd3;
-	return (pipe1);
+	context = ft_calloc(1, sizeof(t_data));
+	context->env = env_to_table(envp);
+	context->stdin_backup = dup(STDIN_FILENO);
+	context->stdout_backup = dup(STDOUT_FILENO);
+	context->root = NULL;
+	return (context);
 }
 
-static void	print_tree(t_ast_node *tree)
+void	free_context(t_data *context)
 {
-	if (tree == NULL)
-		return ;
-	if (tree->type.base == NODE_PIPE)
-	{
-		ft_printf("PIPE:\n");
-		print_tree((t_ast_node *)tree->left);
-		print_tree((t_ast_node *)tree->right);
-	}
-	if (tree->type.base == NODE_COMMAND)
-	{
-		t_command	*cmd = (t_command *)tree;
-		t_list		*node = cmd->redirects;
-		char 		**args = cmd->args;
-		t_redir 	*red;
-
-		ft_printf("  CMD:\n");
-		if (args != NULL)
-			while (*args)
-				ft_printf("\t%s\n", *args++);
-		if (node)
-		{
-			while (node)
-			{
-				red = (t_redir *)node->content;
-				ft_putendl_fd(red->filename, 1);
-				node = node->next;
-			}
-		}
-	}
-}
-
-void print_hash_table(t_hash_table *table)
-{
-    if (!table || !table->items)
-    {
-        printf("Tabela vazia ou não inicializada.\n");
-        return;
-    }
-
-    printf("--- Conteúdo da Tabela Hash (%d/%d itens) ---\n", table->count, table->size);
-
-    for (int i = 0; i < table->size; i++)
-    {
-        t_hash_item *current = table->items[i];
-
-        // Só printamos o índice se ele contiver algum item (opcional)
-        if (current)
-        {
-            printf("Bucket [%d]: ", i);
-            while (current)
-            {
-                printf("[%s = %s (Tag: %s)]", 
-                    current->key, 
-                    current->value ? current->value : "NULL",
-                    current->tag == ENV ? "ENV" : "EXPORT");
-
-                if (current->next)
-                    printf(" -> ");
-                
-                current = current->next;
-            }
-            printf("\n");
-        }
-    }
-    printf("-------------------------------------------\n");
+	free_tree(context->root);
+	context->root = NULL;
+	ft_lstclear(&context->fds, close_fd);
+	ft_lstclear(&context->pids, close_pid);
 }
