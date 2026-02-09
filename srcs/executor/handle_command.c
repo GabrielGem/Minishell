09380a1338509f1bef4947af664c35f99309ca55
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-09 10:14:10 -0300

    fork logic completed

--- srcs/executor/handle_command.c
+++ srcs/executor/handle_command.c
@@ -6,11 +6,12 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 21:22:38 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/04 13:51:40 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/09 09:57:32 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "tests.h"
 
 int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
 	t_data *context);
@@ -22,16 +23,29 @@ void	restore_fd(t_data *context);
 
 int	handle_command(t_ast_node *leaf, t_data *context)
 {
-	int		status;
+	int			status;
+	t_command	*cmd;
+	pid_t		pid;
 
-	handle_command_fd(leaf, STDIN_FILENO, STDOUT_FILENO, context);
-	status = wait_processes(context->pids);
+	cmd = (t_command *)leaf;
+	if (cmd->is_builtin)
+		status = handle_command_fd(leaf, STDIN_FILENO, STDOUT_FILENO, context);
+	else
+	{
+		pid = fork();
+		if (pid == 0)
+			status = handle_command_fd(leaf, STDIN_FILENO, STDOUT_FILENO, context);
+		ft_lstadd_back(&context->pids, ft_lstnew((void *)(long)pid));
+	}
+	if (context->pids != NULL)
+		status = wait_processes(context->pids);
 	return (status);
 }
 
 int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
 	t_data *context)
 {
+	int			status;
 	t_command	*cmd;
 	int			*redirect_file;
 
@@ -44,13 +58,17 @@ int	handle_command_fd(t_ast_node *leaf, int input_fd, int output_fd, \
 			setup_fd(redirect_file[0], STDIN_FILENO);
 		if (redirect_file[1] != -1)
 			setup_fd(redirect_file[1], STDOUT_FILENO);
+		if (redirect_file[0] == 1 || redirect_file[1] == 1)
+			exit(1);
+		free(redirect_file);
 	}
+	status = 0;
 	if (cmd->is_builtin)
-		exec_builtin(context, cmd->args, cmd->is_builtin);
+		status = exec_builtin(context, cmd->args, cmd->is_builtin);
 	else
-		exec_command(context, cmd->args);
+		status = exec_command(context, cmd->args);
 	restore_fd(context);
-	return (0); //TODO
+	return (status);
 }
 
 void	restore_fd(t_data *context)
@@ -63,15 +81,16 @@ void	restore_fd(t_data *context)
 
 int	exec_builtin(t_data *context, char **args, int nb_builtin)
 {
+	int					status;
 	t_builtin			ft;
 	static t_builtin	map[COUNT] = {
 		//&b_echo,
-		NULL,
-		NULL,
-		NULL,
-		NULL,
 		//&b_cd,
 		//&b_pwd,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
 		&b_export,
 		//&b_unset,
 		//&b_env,
@@ -79,27 +98,36 @@ int	exec_builtin(t_data *context, char **args, int nb_builtin)
 	};
 
 	ft = map[nb_builtin];
-	ft(context, args);
-	return (0); //TODO
+	status = ft(context, args);
+	return (status);
 }
 
 int	exec_command(t_data *context, char **args)
 {
+	char	**path;
+	char	*command_path;
 	char	**envp;
-	pid_t	pid;
 
-	pid = fork();
-	if (pid == 0)
+	command_path = args[0];
+	if (!ft_strchr(args[0], '/'))
 	{
-		envp = table_to_envp(context->env);
-		execve(args[0], &args[1], envp);
+		path = ft_split(hash_search(context->env, "PATH", ENV), ':');
+		command_path = binary_search(path, args[0]);
+	}
+	if (!command_path)
+	{
 		ft_putstr_fd("minishell: ", 2);
-		perror(args[0]);
+		ft_putstr_fd(args[0], 2);
+		ft_putstr_fd(": command not found\n", 2);
 		free_context(context);
 		exit(127);
 	}
-	ft_lstadd_back(&context->pids, ft_lstnew((void *)(long)pid));
-	return (0); //TODO
+	envp = table_to_envp(context->env);
+	execve(command_path, &args[1], envp);
+	ft_putstr_fd("minishell: ", 2);
+	perror(args[0]);
+	free_context(context);
+	exit(127);
 }
 
 static void	setup_pipe(int input_fd, int output_fd)
