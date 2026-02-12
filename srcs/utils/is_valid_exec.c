4782789e6a598222d4ce9baef825793de7355fd0
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-12 19:12:15 -0300

    wip: command validation

--- srcs/utils/is_valid_exec.c
+++ srcs/utils/is_valid_exec.c
@@ -6,13 +6,85 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/23 10:26:49 by gabrgarc          #+#    #+#             */
-/*   Updated: 2025/12/23 17:24:48 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/12 18:01:39 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	is_valid_executable(char *path)
+int	file_exist(char *path)
 {
-	return (access(path, F_OK) == 0 && access(path, X_OK) == 0);
+	return (access(path, F_OK) == 0);
+}
+
+int	is_executable(char *path)
+{
+	return (access(path, X_OK) == 0);
+}
+
+int	is_directory(char *path)
+{
+	struct stat	buffer;
+
+	if (stat(path, &buffer) == 0 && S_ISDIR(buffer.st_mode) > 0)
+		return (1);
+	return (0);
+}
+
+int	exit_error(char *cmd, int code, t_data *context)
+{
+	if (code == 127)
+	{
+		if (!ft_strchr(cmd, '/'))
+		{
+			ft_putstr_fd(cmd, 2);
+			ft_putstr_fd(": command not found\n", 2);
+		}
+		else
+		{
+			ft_putstr_fd("minishell: ", 2);
+			perror(cmd);
+		}
+	}
+	else
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(cmd, 2);
+		if (is_directory(cmd))
+			ft_putstr_fd(": Is a directory\n", 2);
+		else
+			ft_putstr_fd(": Permission denied\n", 2);
+	}
+	free_shell(context);
+	exit(code);
+}
+
+char	*get_path(char *cmd, t_data *context)
+{
+	char	*command_path;
+	char	**path;
+
+	if (ft_strchr(cmd, '/'))
+		command_path = cmd;
+	else
+	{
+		path = ft_split(hash_search(context->env, "PATH", ENV), ':');
+		command_path = binary_search(path, cmd);
+		ft_free_split(path);
+	}
+	return (command_path);
+}
+
+int	valid_command(char *cmd)
+{
+	int		error_code;
+
+	error_code = 0;
+	if (!file_exist(cmd))
+		error_code = 127;
+	else if (is_directory(cmd))
+		error_code = 126;
+	else if (!is_executable(cmd))
+		error_code = 126;
+	return (error_code);
 }
