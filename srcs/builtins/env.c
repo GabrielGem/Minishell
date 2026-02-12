c748ffa472cb76ed1781f5048520488311945497
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-11 21:58:23 -0300

    fix: access items

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 14:12:36 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/09 18:36:51 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/11 19:03:39 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 int	b_env(t_data *context, char **args)
 {
-	t_hash_item	*item;
-	int			i;
+	t_hash_item		*item;
+	t_hash_table	*table;
+	int				i;
 
 	(void)args;
+	table = context->env;
 	i = 0;
 	while (i < table->size)
 	{
 		item = table->items[i];
 		while (item)
 		{
-			if (items[i]->tag == ENV)
-				printf("%s=\"%s\"\n", items[i]->key, items[i]->value);
+			if (item[i].tag == ENV)
+				printf("%s=\"%s\"\n", item[i].key, item[i].value);
 			item = item->next;
 		}
 		i++;
