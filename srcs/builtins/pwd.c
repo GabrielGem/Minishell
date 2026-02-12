9c2ffc766fcfb29cae867c8da868ea988ff85877
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-11 21:57:10 -0300

    casting unused parameters

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 12:20:15 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/09 12:36:06 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/11 19:04:38 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	b_pwd(t_data *context, char **args)
 {
 	char	*pwd;
 
+	(void)context;
+	(void)args;
 	pwd = getcwd(NULL, 0);
 	ft_putendl_fd(pwd, STDOUT_FILENO);
 	free(pwd);
