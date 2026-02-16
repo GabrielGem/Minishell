d015e9b8185aeddf1d1fda65f0ffce529a0e9df8
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-16 16:08:10 -0300

    wip: crossing the tree seeking heredoc

--- srcs/executor/heredoc/heredoc.c
+++ srcs/executor/heredoc/heredoc.c
@@ -6,24 +6,24 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 11:49:45 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/10 11:49:46 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/16 14:42:16 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	process_heredoc(char *delimiter, t_data *context)
+int	process_heredoc(char *delimiter, char *temp_file, t_data *context)
 {
 	char	*line;
 	int		fd;
 	int		len;
 
-	fd = open("/tmp/.heredoc_tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	fd = open(temp_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	len = ft_strlen(delimiter);
 	while (1)
 	{
-		ft_putstr_fd("> ", context->stdout_backup);
-		line = get_next_line(context->stdin_backup);
+		ft_putstr_fd("> ", 1);
+		line = get_next_line(0);
 		if (!line)
 			break ;
 		if (ft_strncmp(line, delimiter, len) == 0 && line[len] == '\n')
@@ -31,11 +31,13 @@ int	process_heredoc(char *delimiter, t_data *context)
 			free(line);
 			break ;
 		}
+		// if must expand
 		ft_putstr_fd(line, fd);
 		free(line);
 	}
 	close(fd);
-	fd = open("/tmp/.heredoc_tmp", O_RDONLY);
-	unlink("/tmp/.heredoc_tmp");
+	fd = open(temp_file, O_RDONLY);
+	unlink(temp_file);
+	free(temp_file);
 	return (fd);
 }
