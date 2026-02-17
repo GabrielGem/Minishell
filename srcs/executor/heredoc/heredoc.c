d51f7dce9f9be983b6252b3dd9941a810a717286
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-17 09:35:06 -0300

    heredoc count implemented

--- srcs/executor/heredoc/heredoc.c
+++ srcs/executor/heredoc/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 11:49:45 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/02/16 18:31:02 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/17 09:30:22 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 void	process_heredoc(char *delimiter, char *temp_file)
 {
-	char	*line;
-	int		fd;
-	int		len;
+	static int	count;
+	int			count_lines;
+	char		*line;
+	int			fd;
+	int			len;
 
+	count_lines = 0;
 	fd = open(temp_file, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	len = ft_strlen(delimiter);
 	while (1)
@@ -25,15 +28,21 @@ void	process_heredoc(char *delimiter, char *temp_file)
 		ft_putstr_fd("> ", 1);
 		line = get_next_line(0);
 		if (!line)
+		{
+			count += 1;
+			printf("\nminishell: warning: here-document at line %d delimited by end-of-file (wanted `%s')\n", count, delimiter);
 			break ;
+		}
 		if (ft_strncmp(line, delimiter, len) == 0 && line[len] == '\n')
 		{
 			free(line);
 			break ;
 		}
+		count_lines++;
 		// if must expand
 		ft_putstr_fd(line, fd);
 		free(line);
 	}
+	count += count_lines;
 	close(fd);
 }
