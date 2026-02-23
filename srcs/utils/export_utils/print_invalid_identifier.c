b05a68f9be736be9a8ae3dd1b94e7d07e6d8a493
GabrielGem <gabrielgarciamendes91@gmail.com>
2026-02-23 20:16:08 -0300

    fixed: print in stderror

--- srcs/utils/export_utils/print_invalid_identifier.c
+++ srcs/utils/export_utils/print_invalid_identifier.c
@@ -6,7 +6,7 @@
 /*   By: gabrgarc <gabrgarc@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 14:56:36 by gabrgarc          #+#    #+#             */
-/*   Updated: 2026/01/31 16:21:06 by gabrgarc         ###   ########.fr       */
+/*   Updated: 2026/02/23 18:59:03 by gabrgarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void	print_invalid_identifier(char *identifier)
 {
-	printf("minishell: export: `%s': not a valid identifier\n", identifier);
+	ft_putstr_fd("minishell: export: `", 2);
+	ft_putstr_fd(identifier, 2);
+	ft_putstr_fd("': not a valid identifier\n", 2);
 }
